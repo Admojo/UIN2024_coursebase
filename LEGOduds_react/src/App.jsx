753926e6093932554d61c7849f87217f255b2671
Andrea <andrea.s.nilsson@hiof.no>
2024-01-30 13:13:13 +0100

    endret kode til å hente inn elementer gjennom components(Header, Cart, ContentPage, Nav)

--- LEGOduds_react/src/App.jsx
+++ LEGOduds_react/src/App.jsx
@@ -1,31 +1,18 @@
 import { useState } from 'react'
 import './App.css'
-import { products } from './assets/legoduds'
-import Title from './components/Title'
-import ProductCard from './components/ProductCard'
+import Header from './components/Header'
+import ContentPage from './components/ContentPage'
+import Nav from './components/Nav'
 
 function App() {
 
-
   return (
     <div id="container">
-        <Header/>
-        <nav>
-            <ul>
-                <li><a href="#">City</a></li>
-                <li><a href="#">Ninjago</a></li>
-                <li><a href="#">Castles and Knights</a></li>
-                <li><a href="#">Marine and Pirates</a></li>
-                <li><a href="#">Movie Characters</a></li>
-            </ul>
-        </nav>
-        <main>
-          <Title />
-          {products.map(product => <ProductCard key={product.prodid} category={product.category} title={product.title} img={product.imagefile} price={product.price}/> )}
-        </main>
+      <Header/>
+      <Nav/>
+      <ContentPage />
     </div>
   )
-
 }
 
 export default App
